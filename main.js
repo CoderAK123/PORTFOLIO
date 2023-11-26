b31a4184801aeaef9eb72ae41ec69f070d2ff9d8
CoderAK123 <113004159+CoderAK123@users.noreply.github.com>
2023-11-27 04:36:08 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer" , "writer" , "Web Developer","Java Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+function myFunction() {
+    var x = document.getElementById("more-info");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
